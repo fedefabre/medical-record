1d13f9fd9d4d39a677b7d978b2685f69f27fda90
Federico-Fabre <federico.fabre@xpo.com>
2022-05-13 17:53:03 -0300

    MR-1 - Switch environment to develop

--- src/Api/MedicalRecordDB.ts
+++ src/Api/MedicalRecordDB.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export const MedicalRecordApi = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  // baseURL: 'http://localhost:8080/api',
+  baseURL: 'https://medical-record-mern.herokuapp.com/api',
 })
