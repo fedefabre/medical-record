f3219bd06072ad35a8190cd1f612def02d3dcf31
fedefabre <federicofabre@gmail.com>
2022-06-10 11:12:20 -0300

    MR-1 - Back button implemented on Main

--- src/Skeleton/Sidebar/Main.tsx
+++ src/Skeleton/Sidebar/Main.tsx
@@ -1,17 +1,22 @@
 import { IconButton } from '@chakra-ui/react'
 import { FiArrowLeft } from 'react-icons/fi'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation, useNavigate } from 'react-router-dom';
 
 export default function Main() {
-  
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+  const isADeepRoute = pathname.split('/').length > 2;
 
   return (
     <>
-      <IconButton
+      {/* { Nagivate back only when you are on a child route} */}
+      {isADeepRoute && <IconButton
         variant='outline'
         colorScheme='teal'
         aria-label='Call Sage'
-        icon={<FiArrowLeft />} />
+        icon={<FiArrowLeft />}
+        onClick={() => navigate(-1)}
+      />}
       <Outlet />
     </>
   )
